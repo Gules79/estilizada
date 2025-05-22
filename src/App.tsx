c79d0617322fa3c7aed46fd35d6779d4a1fe91b6
Gules79 <mgules5@gmail.com>
2025-05-22 11:00:50 -0300

    versao sem simbolo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import logo from './inter.png'; // Verifique se o arquivo está na pasta src/
 function App() {
   return (
     <div className="container">
-      <h1>🏆 Inter Campeão! 🟥⬛</h1>
+      <h1> Inter Campeão! </h1>
       <img src={logo} alt="Símbolo do Inter" className="logo" />
     </div>
   );
